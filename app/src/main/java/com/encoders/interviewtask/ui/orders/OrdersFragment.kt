bece9f1eeb13ff6248c61c65c3bbb664d4fe5ea7
Sadeeq Rahman <35709246+sadeeqrahman@users.noreply.github.com>
2022-05-16 19:47:56 +0500

    interview task

---
+++ app/src/main/java/com/encoders/interviewtask/ui/orders/OrdersFragment.kt
@@ -0,0 +1,44 @@
+package com.encoders.interviewtask.ui.orders
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.encoders.interviewtask.databinding.FragmentOrdersBinding
+
+class OrdersFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: OrdersViewModel
+    private var _binding: FragmentOrdersBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(OrdersViewModel::class.java)
+
+        _binding = FragmentOrdersBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
